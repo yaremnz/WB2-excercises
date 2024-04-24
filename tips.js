624f573b1d633669aa626fe1eb12ea14f44c5af6
Yareli Cerda <yarelic.1002@gmail.com>
2024-04-24 14:02:51 -0400

    adding decimal point to tip

---
+++ tips.js
@@ -0,0 +1,12 @@
+// sample inouts
+let foodCost = 79.25;
+let tax = 6.54;
+let tip = 12.00;
+
+// calculations
+let totalDue= foodCost + tax + tip;
+
+// output
+console.log("Food cost is " + foodCost + " and tax is " + tax);
+console.log("Tip is " + tip.toFixed(2))
+console.log("Total Due is " + totalDue);
